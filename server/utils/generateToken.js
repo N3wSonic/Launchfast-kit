03c07f6240287fef00c35d94c0b6bca2f13fb804
CorentinDWWM <newsonic62@gmail.com>
2026-03-09 16:32:56 +0100

    add code example to generateToken.js

---
+++ server/utils/generateToken.js
@@ -0,0 +1,9 @@
+// LaunchFast Kit - Example File
+// Full implementation available in the paid version
+// https://n3wsonic.gumroad.com/l/lksgmo
+
+import jwt from "jsonwebtoken";
+
+export const generateToken = (user) => {
+  // Create a token
+};
